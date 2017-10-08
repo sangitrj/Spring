47d848e52a40887f1f9e9b1dde688222f0590aad
sound <sound@sound-PC>
2017-10-07 21:37:40 -0700

    json

--- Spring/src/spring/test/HelloWorld.java
+++ Spring/src/spring/test/HelloWorld.java
@@ -1,17 +1,29 @@
 package spring.test;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-
+import gvjava.org.json.JSONException;
+import gvjava.org.json.JSONObject;
 import spring.bean.Employee;  
 
 public class HelloWorld {  
-	public static void main(String[] args) {  
-		Resource resource=new ClassPathResource("spring/config/applicationContext.xml");  
+	public static void main(String[] args) throws JSONException {
+		Employee employee = new Employee();
+		employee.setEId("123");
+		employee.setEname("sangit");
+		
+		Employee employee1 = new Employee();
+		employee1.setEId("123");
+		employee1.setEname("sangit");
+		
+		JSONObject jsonObject = new JSONObject(employee);
+		JSONObject jsonObject1 = new JSONObject(employee1);
+		System.out.println(jsonObject.toString());
+		System.out.println(jsonObject.put("emp", jsonObject1));
+		
+		
+		/*Resource resource=new ClassPathResource("spring/config/applicationContext.xml");  
+		
 		BeanFactory factory=new XmlBeanFactory(resource);  
 
 		Employee employee=(Employee)factory.getBean("employeebean");  
-		System.out.println(employee.getEname());
+		System.out.println(employee.getEname());*/
 	}  
 }  
